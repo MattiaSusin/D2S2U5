edc2cb3d8dbc5b20b8622c366b7672bbc69dfbb6
MattiaSusin <m.susin28@gmail.com>
2024-09-10 14:50:27 +0200

    Initial commit

---
+++ src/main/java/mattiasusin/D2S2U5/D2S2U5Application.java
@@ -0,0 +1,14 @@
+package mattiasusin.D2S2U5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2S2U5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D2S2U5Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/mattiasusin/D2S2U5/D2S2U5ApplicationTests.java
@@ -0,0 +1,13 @@
+package mattiasusin.D2S2U5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2S2U5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
